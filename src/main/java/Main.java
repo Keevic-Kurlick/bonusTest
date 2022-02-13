f53e0f5a0933a552110d84c37dca8f097dff2fde
Keevic-Kurlick <tyan.yumiko1@gmail.com>
2022-02-13 21:26:22 +0300

    updated main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         BonusService service = new BonusService();
-        long bonus = service.calculate(1500, true);
-        System.out.println(bonus);
+
+        // подготавливаем данные:
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        // если true - то PASS
+        // если false - то FAIL
+        boolean passed = expected == actual;
+
+        // выводим результат
+        System.out.println(passed);
     }
 }
